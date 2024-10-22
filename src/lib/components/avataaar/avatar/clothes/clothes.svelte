95546b6e900ee7147a07972d400cf54531c95c9e
hisku <hiskias.melke1@gmail.com>
2024-10-22 14:16:26 +0900

    Implement all clothes

--- src/lib/components/avataaar/avatar/clothes/clothes.svelte
+++ src/lib/components/avataaar/avatar/clothes/clothes.svelte
@@ -7,21 +7,21 @@
 	import CollarSweater from './CollarSweater.svelte';
 	import GraphicShirt from './GraphicShirt.svelte';
 	import Hoodie from './Hoodie.svelte';
-	// import Overall from './Overall.svelte';
-	// import ShirtCrewNeck from './ShirtCrewNeck.svelte';
-	// import ShirtScoopNeck from './ShirtScoopNeck.svelte';
-	// import ShirtVNeck from './ShirtVNeck.svelte';
+	import Overall from './Overall.svelte';
+	import ShirtCrewNeck from './ShirtCrewNeck.svelte';
+	import ShirtScoopNeck from './ShirtScoopNeck.svelte';
+	import ShirtVNeck from './ShirtVNeck.svelte';
 
 	const components = [
 		{ component: BlazerShirt, optionValue: 'BlazerShirt' },
 		{ component: BlazerSweater, optionValue: 'BlazerSweater' },
 		{ component: CollarSweater, optionValue: 'CollarSweater' },
 		{ component: GraphicShirt, optionValue: 'GraphicShirt' },
-		{ component: Hoodie, optionValue: 'Hoodie' }
-		//   { component: Overall, optionValue: 'Overall' },
-		//   { component: ShirtCrewNeck, optionValue: 'ShirtCrewNeck' },
-		//   { component: ShirtScoopNeck, optionValue: 'ShirtScoopNeck' },
-		//   { component: ShirtVNeck, optionValue: 'ShirtVNeck' },
+		{ component: Hoodie, optionValue: 'Hoodie' },
+		{ component: Overall, optionValue: 'Overall' },
+		{ component: ShirtCrewNeck, optionValue: 'ShirtCrewNeck' },
+		{ component: ShirtScoopNeck, optionValue: 'ShirtScoopNeck' },
+		{ component: ShirtVNeck, optionValue: 'ShirtVNeck' }
 	];
 
 	const option = ClotheOption;
